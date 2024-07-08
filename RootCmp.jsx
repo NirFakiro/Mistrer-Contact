a496b6c6f5573979ad815dbe4d1fbdebadc076f1
Bar Ben Haim <barbenbh@gmail.com>
2024-07-08 15:46:38 +0300

    Add footer

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,4 +1,5 @@
 import { ContactDetails } from './cmps/ContactDetails.jsx'
+import { Footer } from './cmps/Footer.jsx'
 import { ContactIndex } from './pages/Contactindex.jsx'
 import { Home } from './pages/Home.jsx'
 
@@ -6,15 +7,16 @@ const Router = ReactRouterDOM.HashRouter
 const { Routes, Route } = ReactRouterDOM
 
 export function RootCmp() {
-  return (
-    <section>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact/details" element={<ContactDetails />} />
-          <Route path="/contact/index" element={<ContactIndex />} />
-        </Routes>
-      </Router>
-    </section>
-  )
+    return (
+        <section>
+            <Router>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/contact/details' element={<ContactDetails />} />
+                    <Route path='/contact/index' element={<ContactIndex />} />
+                </Routes>
+                <Footer />
+            </Router>
+        </section>
+    )
 }
